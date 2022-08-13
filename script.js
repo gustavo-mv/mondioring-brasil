f2f4b8e870d43b07ac1c970d346d539922637718
Gustavo M. Vieira <47462562+gustavo-mv@users.noreply.github.com>
2022-08-13 15:19:43 -0300

    Commit Página Inicial quase concluída

--- script.js
+++ script.js
@@ -26,3 +26,14 @@ function start() {
 }
 
 window.addEventListener("load", start)
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0px";
+  } else {
+    document.getElementById("nav").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
