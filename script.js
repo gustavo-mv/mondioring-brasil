f71376cd2611c76f62b96f9798e8e5bd32e3cff0
Gustavo M. Vieira <47462562+gustavo-mv@users.noreply.github.com>
2022-09-13 16:34:33 -0300

    Tela de Login criada e totalmente estilizada

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
 let time = 5000,
-    currentImageIndex = 0,
-    images = document
-                .querySelectorAll("#fotos img")
-    max = images.length;
+  currentImageIndex = 0,
+  images = document.querySelectorAll("#fotos img");
+max = images.length;
 
 function nextImage() {
+  images[currentImageIndex].classList.remove("selected");
 
-    images[currentImageIndex]
-        .classList.remove("selected")
+  currentImageIndex++;
 
-    currentImageIndex++
+  if (currentImageIndex >= max) currentImageIndex = 0;
 
-    if(currentImageIndex >= max)
-        currentImageIndex = 0
-
-    images[currentImageIndex]
-        .classList.add("selected")
+  images[currentImageIndex].classList.add("selected");
 }
 
 function start() {
-    setInterval(() => {
-        // troca de image
-        nextImage()
-    }, time)
+  setInterval(() => {
+    // troca de image
+    nextImage();
+  }, time);
 }
 
-window.addEventListener("load", start)
+window.addEventListener("load", start);
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("nav").style.top = "0px";
   } else {
     document.getElementById("nav").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
-}
+};
